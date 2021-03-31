a0a0cfa2b05a4d1e45d2096782bb3cd08c8e7449
Jod F. Pierre <fedletpierre15@gmail.com>
2021-03-31 19:13:58 -0300

    Leitura do arquivo CSV, tratamento das linhas e inserção no postgres usando C++

--- TRANSACTION/db_transaction.cpp
+++ TRANSACTION/db_transaction.cpp
@@ -11,9 +11,12 @@ TO compile  g++ db_transaction.cpp -o test -lpq
 #include <chrono>
 #include <fstream>
 #include <ctime>
+#include <string>
+#include <bits/stdc++.h>
 #include <postgresql/libpq-fe.h>
 
 using namespace std;
+using std::string;
 
 void rollback(PGconn *conn, PGresult *res){
     res = PQexec(conn, "ROLLBACK");
@@ -27,27 +30,82 @@ void insert_data_expl(PGconn *conn){
    std::string line = "";
 
    file.open("data.csv", ios::in);
-  
 
-   if (file.is_open()){
-      res = PQexec(conn, "BEGIN");
-      while (getline (file,line) )
-      {
-         string sql = "INSERT INTO public.product VALUES("+line+")";
-         res = PQexec(conn,sql.c_str());
-      }
-      res = PQexec(conn, "COMMIT");
-   
-      if (PQresultStatus(res) != PGRES_COMMAND_OK)
-      {
-         std::cout << "Failed to insert: %s" << PQerrorMessage(conn) << std::endl;
-         rollback(conn, res);
-         PQclear(res);
-      }
-      file.close();
+   if(!file.is_open()){
+      cout << "Unable to open file"<< endl;
+      exit(1);
    }
 
-   else cout << "Unable to open file";
+   int count = 0, countLine = 10005, firstLine = 0;
+
+   res = PQexec(conn, "BEGIN");
+   if (PQresultStatus(res) != PGRES_COMMAND_OK)
+    {
+        fprintf(stderr, "BEGIN command failed: %s", PQerrorMessage(conn));
+        PQclear(res);
+        exit(1);
+    }
+
+   while (getline (file,line) )
+   {         
+      if(firstLine == 0){
+         firstLine = 1;
+      }
+      else{
+         vector<string> v;
+
+         stringstream ss(line);
+      
+         while (ss.good()) {
+            string substr;
+            getline(ss, substr, ',');
+            v.push_back(substr);
+         }
+
+         
+      
+         for (size_t i = 0; i < v.size(); i++){
+            v[0].erase(std::remove(v[0].begin(),v[0].end(),'\"'),v[0].end());
+            v[0].erase(std::remove(v[0].begin(),v[0].end(),'\''),v[0].end());
+
+            int id = countLine+1;
+
+            stringstream ss;
+            ss << id;
+            string idToString, first, endpart, sql;
+
+            idToString = ss.str();
+
+            first = "INSERT INTO public.product VALUES("+idToString+","+"\'";
+
+            endpart = "\')";
+            sql = ""+first+ ""+v[0]+""+endpart;
+
+            cout << sql << endl;
+
+            res = PQexec(conn,sql.c_str());
+
+            if (PQresultStatus(res) != PGRES_COMMAND_OK)
+            {
+               std::cout << "Failed to insert: %s" << PQerrorMessage(conn) << std::endl;
+               rollback(conn, res);
+               PQclear(res);
+            }
+            count++;
+            countLine++;
+            break;
+         }
+      }
+   }
+   res = PQexec(conn, "COMMIT");
+   if (PQresultStatus(res) != PGRES_COMMAND_OK){
+      std::cout << "Failed to insert: %s" << PQerrorMessage(conn) << std::endl;
+      rollback(conn, res);
+      PQclear(res);
+   }
+   std::cout << count << std::endl;
+   
+   file.close();
 }
 
 void insert_data_impl(PGconn *conn){
@@ -55,25 +113,66 @@ void insert_data_impl(PGconn *conn){
    PGresult *res;
    fstream file;
    std::string line = "";
-
-   file.open("data.csv", ios::in);
   
-   if (file.is_open()){
-      while (getline (file,line) )
-      {
-         string sql = "INSERT INTO public.product VALUES("+line+")";
-         res = PQexec(conn,sql.c_str());
-      }
-    
-      if (PQresultStatus(res) != PGRES_COMMAND_OK)
-      {
-         std::cout << "Failed to insert: %s" << PQerrorMessage(conn) << std::endl;
-         PQclear(res);
-      }
-      file.close();
-   }
+   file.open("data.csv", ios::in);
 
-   else cout << "Unable to open file";
+    if(!file.is_open()){
+      cout << "Unable to open file"<< endl;
+      exit(1);
+   }
+  
+   int count = 0, countLine = 0, firstLine = 0;
+
+   while (getline (file,line) )
+   {         
+      if(firstLine == 0){
+         firstLine = 1;
+      }
+      else{
+         vector<string> v;
+
+         stringstream ss(line);
+      
+         while (ss.good()) {
+            string substr;
+            getline(ss, substr, ',');
+            v.push_back(substr);
+         }
+      
+         for (size_t i = 0; i < v.size(); i++){
+            v[0].erase(std::remove(v[0].begin(),v[0].end(),'\"'),v[0].end());
+            v[0].erase(std::remove(v[0].begin(),v[0].end(),'\''),v[0].end());
+
+            int id = count+1;
+
+            stringstream ss;
+            ss << id;
+            string idToString, first, endpart, sql;
+
+            idToString = ss.str();
+
+            first = "INSERT INTO public.product VALUES("+idToString+","+"\'";
+
+            endpart = "\')";
+            sql = ""+first+ ""+v[0]+""+endpart;
+
+            cout << sql << endl;
+
+            res = PQexec(conn,sql.c_str());
+
+            if (PQresultStatus(res) != PGRES_COMMAND_OK)
+            {
+               std::cout << "Failed to insert: %s" << PQerrorMessage(conn) << std::endl;
+               PQclear(res);
+            }
+            count++;
+            break;
+         }
+      }
+   }
+   std::cout << count << std::endl;
+   
+   file.close();
 }
 
 int main(int argc, char* argv[]) {
